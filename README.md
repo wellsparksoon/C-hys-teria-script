1b7b3fb22842b3cfc65f086cd290c1e0b4416013
MisakaNo の 小破站 <misakablog@protonmail.com>
2022-09-25 14:06:03 +0000

    Update README.md, hysteria.sh

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# hysteria 一键安装脚本
+# Hysteria 一键安装管理脚本
 
 ```shell
 wget -N --no-check-certificate https://gitlab.com/misakablog/hysteria-script/-/raw/main/hysteria.sh && bash hysteria.sh
--- hysteria.sh
+++ hysteria.sh
@@ -450,7 +450,7 @@ menu() {
     clear
     check_status
     echo "#############################################################"
-    echo -e "#                   ${RED}Hysteria  一键安装脚本${PLAIN}                  #"
+    echo -e "#                  ${RED}Hysteria 一键安装管理脚本${PLAIN}                #"
     echo -e "# ${GREEN}作者${PLAIN}: MisakaNo の 小破站                                  #"
     echo -e "# ${GREEN}博客${PLAIN}: https://blog.misaka.rest                            #"
     echo -e "# ${GREEN}GitLab${PLAIN}: https://gitlab.com/misakablog                     #"
